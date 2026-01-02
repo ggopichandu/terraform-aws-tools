8c63322cae778530da7dcb2029aeb55567fe7cb9
Gopichand <gummagopichand@gmail.com>
2026-01-02 23:09:59 +0530

    cicd

--- cicd/jenkins-agent.sh
+++ cicd/jenkins-agent.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-
 yum install fontconfig java-21-openjdk -y
+
+# Install Terraform
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+
+# Install Nodejs
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
