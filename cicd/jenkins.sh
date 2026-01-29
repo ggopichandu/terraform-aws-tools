e4d3c31583e55ab9c29f8d57677605c76de9a42d
Gopichand <gummagopichand@gmail.com>
2026-01-29 10:29:36 +0530

    terrafom-aws-tools

--- cicd/jenkins.sh
+++ cicd/jenkins.sh
@@ -11,10 +11,11 @@ xfs_growfs /
 xfs_growfs /var
 xfs_growfs /home
 
-
+# Install Jenkins
+#!/bin/bash
 curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-21-openjdk jenkins -y
+yum install fontconfig java-17-openjdk jenkins -y
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
