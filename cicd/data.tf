54a5e5bee81da762d33ab538a8bc10332ac14a67
Gopichand <gummagopichand@gmail.com>
2026-01-29 08:52:56 +0530

    terrafom-aws-tools

--- cicd/data.tf
+++ cicd/data.tf
@@ -2,20 +2,24 @@ data "aws_ami" "joindevops" {
 
     most_recent = true
     owners = ["973714476881"]
-
     filter {
-        name   = "name"
-        values = ["RHEL-9-DevOps-Practice"]
+        name = "name"
+        values = ["Redhat-9-DevOps-Practice"]
     }
-
+  
     filter {
-        name   = "root-device-type"
+        name = "root-device-type"
         values = ["ebs"]
     }
 
     filter {
-        name   = "virtualization-type"
+        name = "virtualization-type"
         values = ["hvm"]
+    }
+
+    filter {
+        name = "architecture"
+        values = ["x86_64"]
     }
 }
 
